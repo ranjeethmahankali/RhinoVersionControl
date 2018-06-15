000af22c13800487e553621e3c144abd13954bdb
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2018-06-14 17:36:43 -0700

    initial attempts to persist object table information

---
+++ RhinoVersionControl/RvcCore/Util/TableUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.FileIO;
+using Rhino.Runtime;
+
+namespace RvcCore.Util
+{
+    public class TableUtil
+    {
+        /// <summary>
+        /// For some reason Rhino3dmIO tables don't implement the linq methods, so we need this to manually convert stuff
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(IEnumerable<T> table)
+            where T : CommonObject
+            //where TableT: IEnumerable<T>
+        {
+            List<T> list = new List<T>();
+            foreach (var obj in table)
+            {
+                obj.EnsurePrivateCopy();
+                list.Add(obj);
+            }
+            return list;
+        }
+    }
+}
---
+++ RhinoVersionControl/RvcTestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.FileIO;
+using RvcCore.Util;
+using Newtonsoft.Json;
+
+namespace RvcTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ranje\Desktop\testFile.3dm";
+            List<double> nums = new List<double>() { 1,2,3,4,5 };
+            var first = nums.First();
+            List<File3dmObject> objs, newObjs;
+            string oldOne, newOne;
+            using (File3dm file = File3dm.Read(path))
+            {
+                objs = TableUtil.ToList(file.Objects);
+                oldOne = JsonConvert.SerializeObject(objs);
+            }
+            Console.WriteLine("Please edit the contents of the file and save it");
+            Console.ReadKey();
+            using (File3dm file = File3dm.Read(path))
+            {
+                newObjs = TableUtil.ToList(file.Objects);
+                newOne = JsonConvert.SerializeObject(newObjs);
+            }
+
+            objs = JsonConvert.DeserializeObject<List<File3dmObject>>(oldOne);
+            newObjs = JsonConvert.DeserializeObject<List<File3dmObject>>(newOne);
+        }
+    }
+}
