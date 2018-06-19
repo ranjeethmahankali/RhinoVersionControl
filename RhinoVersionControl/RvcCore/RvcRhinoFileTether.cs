fc2c6f794f30ef82cfa268ccd61084a43bf6f666
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2018-06-18 21:07:20 -0700

    added the top level classes to link this class library to a rhino plugin (yet to be developed)

---
+++ RhinoVersionControl/RvcCore/RvcRhinoFileTether.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RvcCore
+{
+    /// <summary>
+    /// An instance of this class will represent the connection between a Rhino file and an rvc archive that is managing the versions
+    /// of that file. This instance will be serialized and saved to the string table of the Rhino doc.
+    /// </summary>
+    public class RvcRhinoFileTether
+    {
+        //incomplete
+    }
+}
---
+++ RhinoVersionControl/RvcCore/Util/FileUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RvcCore.Util
+{
+    /// <summary>
+    /// This class has the top level methods to process rhino files
+    /// </summary>
+    public class FileUtil
+    {
+        //incomplete
+    }
+}
